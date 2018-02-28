b63e5aadc776023e69326d307cb5110f015ece3a
Constantino Schillebeeckx <titous@gmail.com>
2018-02-28 08:27:24 -0600

    add favicon

--- oolong_app/db_init.sql
+++ oolong_app/db_init.sql
@@ -2,6 +2,8 @@ INSERT INTO activity (name, description) VALUES
 ('eat','eat'),
 ('sleep','sleep');
 INSERT INTO public.auth_user
-(password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
-VALUES('pbkdf2_sha256$36000$usemzdFnEivt$Wpb4Z2zJq/YUrg8FFN+y/qzB20pDeJfqg2CE24ey5nc=', NULL, true, 'constantino', 'Constantino', 'Schillebeeckx', 'titou@gmail.com', true, true, '2018-02-26 17:12:31.240');
+(password, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined) VALUES
+('pbkdf2_sha256$36000$usemzdFnEivt$Wpb4Z2zJq/YUrg8FFN+y/qzB20pDeJfqg2CE24ey5nc=', NULL, true, 'constantino', 'Constantino', 'Schillebeeckx', 'titou@gmail.com', true, true, '2018-02-26 17:12:31.240'),
+('pbkdf2_sha256$36000$wLBJNgE16iS4$1NFzUVoGAmIq6phn61H0LDbeJrcZHSOLbxfzpiivkwc=', '2018-02-27 16:42:57.534', false, 'meow', 'meow', 'meow', 'meow@gmail.com', false, true, '2018-02-27 16:42:57.449');
+
 
--- oolong_app/urls.py
+++ oolong_app/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import include, url
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from . import views
 
@@ -8,4 +10,11 @@ urlpatterns = [
     url(r'^login/$', views.user_login, name='login'),
     url(r'^signup/$', views.user_signup, name='signup'),
     url(r'^log_out/$', views.user_logout, name='log_out'),
+    url(
+        r'^favicon.ico$',
+        RedirectView.as_view(
+            url=staticfiles_storage.url('favicon.ico'),
+            permanent=False),
+        name="favicon"
+    ),
 ]
